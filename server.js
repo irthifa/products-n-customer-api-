7e868a9e180de9717a18971a11cc7f7c26b51205
irthifa <96048806+irthifa@users.noreply.github.com>
2023-04-26 23:56:30 +0530

    major changes - final draft

--- server.js
+++ server.js
@@ -20,270 +20,7 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
-app.get("/api/products", (req, res, next) => {
-    try {
-        var sql = "select * from products"
-        var params = []
-        db.all(sql, params, (err, rows) => {
-            if (err) {
-                res.status(400).json({ "error": err.message });
-                return;
-            }
-            res.json({
-                "message": "success",
-                "data": rows
-            })
-        });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-
-});
-
-app.get("/api/products/:id", (req, res, next) => {
-    try {
-        var sql = "select * from products where id = ?"
-        var params = [req.params.id]
-        db.get(sql, params, (err, row) => {
-            if (err) {
-                res.status(400).json({ "error": err.message });
-                return;
-            }
-            res.json({
-                "message": "success",
-                "data": row
-            })
-        });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-});
-
-app.get("/api/products/quantity/:quantity", (req, res, next) => {
-    try {
-        var row = "[]"
-        var sql = "select * from products where quantity > ?"
-        var params = [req.params.quantity]
-        db.get(sql, params, (err, row) => {
-            if (err) {
-                res.status(400).json({ "error": err.message });
-                return;
-            }
-            res.json({
-                "message": "success",
-                "data": row
-            })
-        });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-});
-
-app.get("/api/products/unitPrice/:unitPrice", (req, res, next) => {
-    try {
-        var row = "[]"
-        var sql = "select * from products where unitPrice > ?"
-        var params = [req.params.unitPrice]
-        db.get(sql, params, (err, row) => {
-            if (err) {
-                res.status(400).json({ "error": err.message });
-                return;
-            } else {
-                res.json({
-
-                    "message": "success",
-                    "data": row
-                })
-            }
-
-        });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-});
-
-app.post("/api/products/", (req, res, next) => {
-
-    try {
-        var errors = []
-
-        if (!req.body) {
-            errors.push("An invalid input");
-        }
-
-        const { productName,
-            description,
-            category,
-            brand,
-            expiredDate,
-            manufacturedDate,
-            batchNumber,
-            unitPrice,
-            quantity,
-            createdDate
-        } = req.body;
-
-        var sql = 'INSERT INTO products (productName, description, category, brand, expiredDate, manufacturedDate, batchNumber, unitPrice, quantity, createdDate) VALUES (?,?,?,?,?,?,?,?,?,?)'
-        var params = [productName, description, category, brand, expiredDate, manufacturedDate, batchNumber, unitPrice, quantity, createdDate]
-        db.run(sql, params, function (err, result) {
-
-            if (err) {
-                res.status(400).json({ "error": err.message })
-                return;
-            } else {
-                res.json({
-                    "message": "success",
-                    "data": req.body,
-                    "id": this.lastID
-                })
-            }
-
-        });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-});
-
-
-app.put("/api/products/", (req, res, next) => {
-
-
-    const {
-        id,
-        productName,
-        description,
-        category,
-        brand,
-        expiredDate,
-        manufacturedDate,
-        batchNumber,
-        unitPrice,
-        quantity,
-        createdDate
-    } = req.body;
-
-    db.run(`UPDATE products set productName = ?, description = ?, category = ?, brand = ?,expiredDate=?,manufacturedDate=?,batchNumber=?,unitPrice=?,quantity=?,createdDate=? WHERE id = ?`,
-        [productName, description, category, brand, expiredDate, manufacturedDate, batchNumber, unitPrice, quantity, createdDate, id],
-        function (err, result) {
-            if (err) {
-                res.status(400).json({ "error": res.message })
-                return;
-            }
-            res.status(200).json({ updated: this.changes });
-        });
-});
-
-
-app.delete("/api/products/delete/:id", (req, res, next) => {
-    try {
-        db.run(
-            'DELETE FROM products WHERE id = ?',
-            req.params.id,
-            function (err, result) {
-                if (err) {
-                    res.status(400).json({ "error": res.message })
-                    return;
-                }
-                res.json({ "message": "deleted", rows: this.changes })
-            });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-});
-
-app.delete("/api/products/deleteAll/:id", (req, res, next) => {
-    try {
-        db.run(
-            'DELETE FROM products WHERE id > ?',
-            req.params.id,
-            function (err, result) {
-                if (err) {
-                    res.status(400).json({ "error": res.message })
-                    return;
-                }
-                res.json({ "message": "deleted", rows: this.changes })
-            });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-});
-
-
-app.get("/api/suppliers/", (req, res, next) => {
-    try {
-        var sql = "select * from suppliers"
-        var params = []
-        db.all(sql, params, (err, rows) => {
-            if (err) {
-                res.status(400).json({ "error": err.message });
-                return;
-            }
-            res.json({
-                "message": "success",
-                "data": rows
-            })
-        });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-
-});
-
-
-app.post("/api/suppliers/", (req, res, next) => {
-
-    try {
-        var errors = []
-
-        if (!req.body) {
-            errors.push("An invalid input");
-        }
-
-        const { supplierName,
-            address,
-            joinedDate,
-            mobileNo
-        } = req.body;
-
-        var sql = 'INSERT INTO suppliers (supplierName, address, joinedDate, mobileNo) VALUES (?,?,?,?)'
-        var params = [supplierName, address, joinedDate, mobileNo]
-        db.run(sql, params, function (err, result) {
-
-            if (err) {
-                res.status(400).json({ "error": err.message })
-                return;
-            } else {
-                res.json({
-                    "message": "success",
-                    "data": req.body,
-                    "id": this.lastID
-                })
-            }
-
-        });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-});
-
-app.delete("/api/suppliers/deleteAll/:id", (req, res, next) => {
-    try {
-        db.run(
-            'DELETE FROM suppliers WHERE id > ?',
-            req.params.id,
-            function (err, result) {
-                if (err) {
-                    res.status(400).json({ "error": res.message })
-                    return;
-                }
-                res.json({ "message": "deleted", rows: this.changes })
-            });
-    } catch (E) {
-        res.status(400).send(E);
-    }
-});
-
-app.post("/api/customer/", (req, res, next) => {
+app.post("/api/customer/registration/", (req, res, next) => {
 
     try {
         var errors = []
@@ -304,6 +41,20 @@ app.post("/api/customer/", (req, res, next) => {
           cvv, 
           timeStamp
         } = req.body;
+        
+        //validations
+        const emailRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0.9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+        const creditCardregEx = /^\d{12}$/;
+        
+        if(emailRegEx.test(email)!=true){
+            res.status(400).send("Invalid Email Address");
+            return;
+        }
+        
+        if(creditCardregEx.test(cardNumber)!=true){
+            res.status(400).send("Invalid Credit Card Number");
+            return;
+        }
 
         var sql = 'INSERT INTO customer (name, address, email, dateOfBirth, gender, age, cardHolderName, cardNumber, expiryDate, cvv, timeStamp) VALUES (?,?,?,?,?,?,?,?,?,?,?)'
         var params = [name, address, email, dateOfBirth, gender, age, cardHolderName, cardNumber, expiryDate, cvv, timeStamp]
@@ -325,9 +76,7 @@ app.post("/api/customer/", (req, res, next) => {
     }
 });
 
-
-
 // Root path
 app.get("/", (req, res, next) => {
-    res.json({ "message": "University of Moratuwa" })
+    res.json({ "message": "My Edited Version" })
 });
