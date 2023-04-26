1d95a2a7cacc994af979f0bf78c21b7e67c3df19
irthifa <96048806+irthifa@users.noreply.github.com>
2023-04-26 15:57:17 +0530

    added customer table and data

--- database.js
+++ database.js
@@ -11,6 +11,30 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         throw err
     } else {
         console.log('Connected to the SQlite database.')
+        
+        db.run(`CREATE TABLE customer (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
+            address text,
+            email text,
+            dateOfBirth text,
+            gender text,
+            age text,
+            cardHolderName text,
+            cardNumber text,
+            expiryDate text,
+            cvv text,
+            timeStamp text
+            )`, (err) => {
+            if (err) {
+                // Table already created
+            } else {
+                // Table just created, creating some rows
+                var insert = 'INSERT INTO customer (name, address, email, dateOfBirth, gender, age, cardHolderName, cardNumber, expiryDate, cvv, timeStamp) VALUES (?,?,?,?,?,?,?,?,?,?,?)'
+                db.run(insert, ["irthifa ikram", "11/2 A, Wonder Lane, Beautiful City", "myemail@gmail.com", "2000.02.02", "female", "23", "irthifa ikram", "123456789012", "02/2025", "123", "2023-04-25 22:22:22"])
+            }
+        })
+        
         db.run(`CREATE TABLE products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             productName text, 
